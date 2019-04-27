fb9d9ad37afec39bdfb3e76750f5d8e337da7693
Rad <chihaby@gmail.com>
2019-04-27 09:37:03 -0700

    heroku troubleshoot

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-
+mongoose.connect(MONGODB_URI);
+//, { useNewUrlParser: true }
 // Routes
 
 app.get("/", function (req, res) {
