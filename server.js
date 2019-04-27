523463ff51b4022514dd88db3d71d3ef38a4f309
Rad <chihaby@gmail.com>
2019-04-27 10:11:20 -0700

    update2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 var db = require("./models");
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 var app = express();
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
@@ -14,8 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI);
-//, { useNewUrlParser: true }
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
 // Routes
 
 app.get("/", function (req, res) {
